0af36012528a61909e40371cb490ecb28829920c
JoaoVAzevedo <joaovitorazevedo56@gmail.com>
2025-10-23 20:43:43 -0300

    correcao login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { UsuarioService } from '../../services/usuarios/usuarios';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,31 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./login.css']
 })
 export class LoginComponent {
-  nome: string = '';
+  email: string = '';
   senha: string = '';
 
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+
   entrar() {
-    if (!this.nome || !this.senha) {
+    if (!this.email || !this.senha) {
       alert('Preencha todos os campos!');
       return;
     }
 
-    alert(`Bem-vindo, ${this.nome}!`);
+    const sucesso = this.usuarioService.login(this.email, this.senha);
+
+    if (sucesso) {
+      const usuario = this.usuarioService.getUsuarioLogado();
+      const nome = usuario?.nome || this.email;;
+
+
+      alert(`Bem-vindo, ${nome}!`);
+      //this.router.navigate(['/home']); DESCOMENTAR QUANDO HOME E SUA ROTA ESTIVEREM PRONTOS
+    }
+    else {
+      alert('Usuário ou senha inválidos!');
+    }
+
+
   }
 }
