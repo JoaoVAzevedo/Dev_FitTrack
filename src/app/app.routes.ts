d5d52571cf45f3a093ccab340aea58f919b2a4b0
JoaoVAzevedo <joaovitorazevedo56@gmail.com>
2025-10-24 16:27:12 -0300

    criacao rota menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
+import { MenuComponent } from './pages/menu/menu';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'menu', component: MenuComponent }
 ];
